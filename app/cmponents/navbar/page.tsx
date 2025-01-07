c3a6c6835e459c28fde57ffc7104940a45610c08
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-01-08 01:58:58 +0500

    complete

--- app/cmponents/navbar/page.tsx
+++ app/cmponents/navbar/page.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import { FaShoppingCart } from 'react-icons/fa'; // Importing cart icon from React Icons
+import { FaShoppingCart } from 'react-icons/fa';
 import { TbBrandShopee } from "react-icons/tb";
 
-
-interface NavbarProps {
+// Defining the interface for the Navbar props
+export interface NavbarProps {
   cartCount: number; // Prop to receive cart count
 }
 
-const Navbar = ({ cartCount } : NavbarProps) => {
+// Navbar component
+const Navbar = ({ cartCount }: NavbarProps) => {
   return (
     <div className="bg-gray-800 text-white fixed w-full top-0 left-0 z-10 py-4 px-6 flex justify-between items-center shadow-lg">
-
-      <h1 className="text-2xl font-bold flex items-center gap-2">VEGExFRUU <TbBrandShopee/> </h1>
+      <h1 className="text-2xl font-bold flex items-center gap-2">
+        VEGExFRUU <TbBrandShopee/>
+      </h1>
       <div className="flex items-center space-x-4">
         <div className="text-lg flex items-center">
           <FaShoppingCart />
-          <span className="ml-2">{cartCount}</span> 
+          <span className="ml-2">{cartCount}</span>
         </div>
       </div>
     </div>
   );
 };
 
+// Default export for the component
 export default Navbar;
